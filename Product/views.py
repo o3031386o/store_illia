8ec74c83de55d726c739acec067018cb9ae597d8
AmirhosseinSheikh <amiram17py@gmail.com>
2025-08-11 15:47:45 +0330

    home page view

--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.shortcuts import render
+from .models import *
+import random
+
+def homepage(request):
+    subcategory = SubCategory.objects.filter(category__name="مردانه")
+    products = Product.objects.all()
+    recent = products[0:5] if products.count() >= 5 else products
+    random_products = random.sample(products, k=(6 if products.count() >=6 else products.count()))
+    return render(request,template_name='index.html', context={'subcategory': subcategory,
+                                                               'products': random_products,
+                                                               'recent': recent})
+
+def search(request):
+    products = Product.objects.filter(name__icontains=request.GET['search'])
+    color = Colors.objects.all()
+    size = Size.objects.all()
+    return render(request,template_name='searchpage.html', context={'products': products,
+                                                                    'color': color,
+                                                                    'size': size})
+
+
+def product(request,pk):
+    Product.objects.get(pk=pk)
+    return render(request,template_name='product.html', context={'pk': pk})
+
