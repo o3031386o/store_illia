593a1531da108f4b8f79e490aabaf09a05ec377c
Omid Reza <omidrezanabavi@gmail.com>
2025-08-12 16:37:08 +0430

    add template

--- Product/urls.py
+++ Product/urls.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from. views import*
+
 
 urlpatterns = [
-
+    path('',homepage),
+    path('search/',search),
+    path('product/<int:pk>/',product)
 ]
 
 
--- store/urls.py
+++ store/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('Product.urls')),
+
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
