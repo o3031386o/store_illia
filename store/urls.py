31411f3b220ea4b9dc231de346d6e8d2581a0aae
Omid Reza <omidrezanabavi@gmail.com>
2025-09-09 15:06:22 +0430

    test

--- store/urls.py
+++ store/urls.py
@@ -22,8 +22,7 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Product.urls')),
-
-    path('', include("accounts.urls")),
+    path('', include("Account.urls")),
 
 
 ]
